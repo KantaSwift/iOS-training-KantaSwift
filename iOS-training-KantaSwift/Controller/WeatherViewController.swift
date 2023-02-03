5085ddeaf1cca7d703c74861301009546cf8dc7f
上條 栞汰 <k_kamijo@MacBook-Pro.local>
2023-02-03 11:44:29 +0900

    ライフサイクルメソッドの挙動確認とモーダル遷移処理

---
+++ iOS-training-KantaSwift/Controller/WeatherViewController.swift
@@ -0,0 +1,110 @@
+//
+//  ViewController.swift
+//  iOS-training-KantaSwift
+//
+//  Created by 上條 栞汰 on 2023/02/02.
+//
+
+import UIKit
+import SnapKit
+import YumemiWeather
+
+final class ViewController: UIViewController{
+    
+    // MARK: - UI
+    private let wheatherImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemBackground
+        return imageView
+    }()
+    
+    private let blueLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .blue
+        label.text = "--"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let redLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .red
+        label.text = "--"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var reloadButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("reload", for: .normal)
+        button.addTarget(self, action: #selector(getWeatherData), for: .touchUpInside)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
+        return button
+    }()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("close", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
+        return button
+    }()
+    
+    // MARK: - UIStackViews
+    private lazy var labelStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [blueLabel, redLabel])
+        stackView.distribution = .fillEqually
+        stackView.axis = .horizontal
+        return stackView
+    }()
+    
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [reloadButton, closeButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    // MARK: - LifeCycleMethod
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupView()
+        setupConstraint()
+    }
+}
+
+private extension ViewController {
+    private func setupView() {
+        view.addSubview(wheatherImageView)
+        view.addSubview(labelStackView)
+        view.addSubview(buttonStackView)
+    }
+    
+    private func setupConstraint() {
+        wheatherImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.width.equalToSuperview().dividedBy(2)
+            $0.height.equalTo(wheatherImageView.snp.width)
+        }
+        
+        labelStackView.snp.makeConstraints {
+            $0.top.equalTo(wheatherImageView.snp.bottom)
+            $0.width.equalTo(wheatherImageView.snp.width)
+            $0.centerX.equalToSuperview()
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.top.equalTo(labelStackView.snp.bottom).offset(80)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(wheatherImageView.snp.width)
+        }
+    }
+    
+    @objc private func getWeatherData() {
+        let weatherString = YumemiWeather.fetchWeatherCondition()
+        guard let weather = Weather(rawValue: weatherString) else { return }
+        wheatherImageView.image = weather.image
+    }
+}
