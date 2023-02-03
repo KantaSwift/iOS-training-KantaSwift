cc84c954703eea0810b9e8c1cea69ea5589162a7
上條 栞汰 <k_kamijo@MacBook-Pro.local>
2023-02-03 10:47:31 +0900

    プルリクの修正

--- iOS-training-KantaSwift/ViewController.swift
+++ iOS-training-KantaSwift/ViewController.swift
@@ -35,7 +35,7 @@ final class ViewController: UIViewController {
     
     private let reloadButton: UIButton = {
         let button = UIButton()
-        button.setTitle("reload", for: .normal)
+        button.setTitle("Reload", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
         return button
@@ -43,7 +43,7 @@ final class ViewController: UIViewController {
     
     private let closeButton: UIButton = {
         let button = UIButton()
-        button.setTitle("close", for: .normal)
+        button.setTitle("Close", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 22, weight: .bold)
         return button
@@ -57,8 +57,15 @@ final class ViewController: UIViewController {
         return stackView
     }()
     
+    private lazy var centerStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [imageView, labelStackView])
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        return stackView
+    }()
+    
     private lazy var buttonStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [reloadButton, closeButton])
+        let stackView = UIStackView(arrangedSubviews: [closeButton, reloadButton])
         stackView.axis = .horizontal
         stackView.distribution = .fillEqually
         return stackView
@@ -75,27 +82,29 @@ final class ViewController: UIViewController {
 
 private extension ViewController {
     private func setupView() {
-        view.addSubview(imageView)
-        view.addSubview(labelStackView)
+//        view.addSubview(imageView)
+//        view.addSubview(labelStackView)
+        view.addSubview(centerStackView)
         view.addSubview(buttonStackView)
     }
     
     private func setupConstraint() {
-        imageView.snp.makeConstraints {
+        centerStackView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.width.equalToSuperview().dividedBy(2)
-            $0.height.equalTo(imageView.snp.width)
         }
         
+        imageView.snp.makeConstraints {
+            $0.height.equalTo(centerStackView.snp.width)
+        }
+
         labelStackView.snp.makeConstraints {
             $0.top.equalTo(imageView.snp.bottom)
-            $0.width.equalTo(imageView.snp.width)
-            $0.centerX.equalToSuperview()
         }
         
         buttonStackView.snp.makeConstraints {
             $0.top.equalTo(labelStackView.snp.bottom).offset(80)
-            $0.centerX.equalToSuperview()
+            $0.centerX.equalTo(labelStackView.snp.centerX)
             $0.width.equalTo(imageView.snp.width)
         }
     }
