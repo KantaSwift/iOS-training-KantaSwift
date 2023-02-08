9290a5c53c43264e7c8281d5bb8d0c8b4c5cb5e8
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-02-08 17:10:21 +0900

    Codableを利用したAPIパラメータの作成

--- iOS-training-KantaSwift/Model/API/WeatherAPIClient.swift
+++ iOS-training-KantaSwift/Model/API/WeatherAPIClient.swift
@@ -16,18 +16,16 @@ protocol WeatherAPIClientDelegate: AnyObject {
 
 final class WeatherAPIClient {
     
-    private var sampleJson = """
-                            {
-                                "area": "tokyo",
-                                "date": "2020-04-01T12:00:00+09:00"
-                            }
-                            """
+    private let weatherAPIRequest = WeatherAPIRequest(area: "tokyo", date: "2020-04-01T12:00:00+09:00")
     private let decoder  = JSONDecoder()
+    private let encoder = JSONEncoder()
     weak var delegate: WeatherAPIClientDelegate?
 
     func requestWeather() {
         do {
-            let jsonString = try YumemiWeather.fetchWeather(sampleJson)
+            let data  = try encoder.encode(weatherAPIRequest)
+            guard let parameter = String(data: data, encoding: .utf8) else { return }
+            let jsonString = try YumemiWeather.fetchWeather(parameter)
             guard let data = jsonString.data(using: .utf8) else { return }
             let weather = try decoder.decode(WeatherData.self, from: data)
             delegate?.didUpdateWeather(weather)
---
+++ iOS-training-KantaSwift/Model/API/WeatherAPIRequest.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPIRequest.swift
+//  iOS-training-KantaSwift
+//
+//  Created by 上條 栞汰 on 2023/02/08.
+//
+
+import Foundation
+
+struct WeatherAPIRequest: Codable {
+    let area: String
+    let date: String
+}
