f0e0cb01b15563d342f890cf44b3b6b30f7ed7c1
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2023-02-13 15:19:00 +0900

    rebase時のconflict解消

--- iOS-training-KantaSwift/Controller/WeatherViewController.swift
+++ iOS-training-KantaSwift/Controller/WeatherViewController.swift
@@ -121,6 +121,7 @@ private extension WeatherViewController {
 
 
 
+
 // MARK: - DelegateMethods
 extension WeatherViewController: WeatherAPIClientDelegate {
     func weatherAPIClient(didFailWithError: YumemiWeatherError) {
--- iOS-training-KantaSwift/Model/API/WeatherAPIClient.swift
+++ iOS-training-KantaSwift/Model/API/WeatherAPIClient.swift
@@ -7,6 +7,7 @@
 
 import YumemiWeather
 
+
 protocol WeatherAPIClientDelegate: AnyObject {
     func didUpdateWeather(_ weather: String)
     func weatherAPIClient(didFailWithError error: YumemiWeatherError)
